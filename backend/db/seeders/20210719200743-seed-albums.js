1c1e5dcca4554f221005d00b294c2b78b38d6bc0
jojo <johnathanyih@gmail.com>
2021-07-23 14:01:12 -0700

    updated seeders

--- backend/db/seeders/20210719200743-seed-albums.js
+++ backend/db/seeders/20210719200743-seed-albums.js
@@ -18,6 +18,31 @@ module.exports = {
         title: 'Mumford & Sons - I Will Wait (Bloombox & Sam Feldt Remix)',
         imageUrl: 'https://nimbus-sounds.s3.us-west-1.amazonaws.com/album-art/i-will-wait-mumford-and-sons-bloombox-and-sam-feldt-remix.jpg',
       },
+      {
+        userId: 4,
+        title: 'LA PA PARADISE',
+        imageUrl: 'https://static.qobuz.com/images/covers/67/67/0190295736767_600.jpg',
+      },
+      {//5
+        userId: 4,
+        title: 'Joyful Style',
+        imageUrl: 'https://m.media-amazon.com/images/I/710WR5n2vCL._SS500_.jpg',
+      },
+      {
+        userId: 4,
+        title: 'FREEDOM',
+        imageUrl: 'https://m.media-amazon.com/images/I/710WR5n2vCL._SS500_.jpg',
+      },
+      {
+        userId: 5,
+        title: 'Bae 5',
+        imageUrl: 'https://f4.bcbits.com/img/a1514091039_10.jpg',
+      },
+      {
+        userId: 6,
+        title: 'Yakuza Zero OST',
+        imageUrl: 'https://lyricstranslate.com/files/styles/large/public/images-7_0.jpeg',
+      },
     ], {});
   },
 
